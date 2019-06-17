02c3f7e9d5dee45227102b31dfc678f8087ed2a0
insanefury <justdarsan@gmail.com>
2019-06-17 05:35:40 -0300

    Added camera zoom on landing

---
+++ Assets/Game/Scripts/Player/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("CamSettings")]
+    public float minAltitudeToZoom = 20f;
+    public Vector3 offset;
+    public GameObject sCam;
+
+    Player player;
+
+    void Start()
+    {
+        player = Player.Get();   
+    }
+
+    void Update()
+    {
+        ZoomToPlayer();
+    }
+
+    void ZoomToPlayer()
+    {
+        if (player.altitude <= minAltitudeToZoom)
+        {
+            sCam.SetActive(true);
+        }
+        else if (player.altitude > minAltitudeToZoom)
+        {
+            sCam.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
